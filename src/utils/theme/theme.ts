5fc0bb119984dcb441a1a9453b90e476b0590797
김진근 <freedom7113@gmail.com>
2022-01-12 20:21:10 +0900

    #4 Modify add color

--- src/utils/theme/theme.ts
+++ src/utils/theme/theme.ts
@@ -11,6 +11,34 @@ interface Font {
   description2: string;
 }
 
+interface Color {
+  background: string;
+  primary: {
+    default: string;
+    click: string;
+    opacity38: string;
+    opacity28: string;
+    opacity17: string;
+    opacity7: string;
+  };
+  red: {
+    default: string;
+    click: string;
+  };
+  grayscale: {
+    scale10: string;
+    scale20: string;
+    scale30: string;
+    scale40: string;
+    scale50: string;
+    scale60: string;
+    scale70: string;
+    scale80: string;
+    scale90: string;
+    scale100: string;
+  };
+}
+
 const fonts: Font = {
   h1: `bold 2rem/130% 'Spoqa Han Sans Neo', 'sans-serif'`,
   h2: `bold 1.75rem/130% 'Spoqa Han Sans Neo', 'sans-serif'`,
@@ -22,6 +50,34 @@ const fonts: Font = {
   body3: `400 1rem/130% 'Spoqa Han Sans Neo', 'sans-serif'`,
   description1: `400 0.875rem/130% 'Spoqa Han Sans Neo', 'sans-serif'`,
   description2: `400 0.75rem/130% 'Spoqa Han Sans Neo', 'sans-serif'`,
+};
+
+const colors: Color = {
+  background: "#F3F2F7",
+  primary: {
+    default: "#7366EF",
+    click: "#5B50BD",
+    opacity38: "#9D94EF",
+    opacity28: "#B3ACEF",
+    opacity17: "#CAC6EF",
+    opacity7: "#E0DEEF",
+  },
+  red: {
+    default: "#F85555",
+    click: "#C44343",
+  },
+  grayscale: {
+    scale10: "#FFFFFF",
+    scale20: "#EEEDF2",
+    scale30: "#C9C8CF",
+    scale40: "#B0B0B5",
+    scale50: "#97979C",
+    scale60: "#7F7E82",
+    scale70: "#666569",
+    scale80: "#4D4D4F",
+    scale90: "#313036",
+    scale100: "#17171C",
+  },
 };
 
 export {};
