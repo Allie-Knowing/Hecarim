d2f82bb63ba31f9469f9652b376204cdb6d33ada
김진근 <freedom7113@gmail.com>
2022-01-14 20:07:29 +0900

    #8 Modify add props in outline input

--- src/components/Inputs/Outline/index.tsx
+++ src/components/Inputs/Outline/index.tsx
@@ -1,14 +1,16 @@
 import useFocus from "hooks/useFocus";
 import { useContext } from "react";
+import { TextInput, TextInputProps } from "react-native";
 import { ThemeContext } from "styled-components/native";
 import * as S from "./styled";
 
-const OutlineInput = () => {
+const OutlineInput = (props: TextInputProps) => {
   const [inputProps, isFocus] = useFocus();
   const themeContext = useContext(ThemeContext);
 
   return (
     <S.Input
+      {...(props as TextInput)}
       {...inputProps}
       {...{ isFocus }}
       placeholder="hello wo"
