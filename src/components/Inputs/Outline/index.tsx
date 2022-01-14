4f889b2a6fc16346e9c7751e11ba80f28a9d0794
김진근 <freedom7113@gmail.com>
2022-01-14 18:51:48 +0900

    #8 Modify realize Outline Input

--- src/components/Inputs/Outline/index.tsx
+++ src/components/Inputs/Outline/index.tsx
@@ -1,7 +1,20 @@
+import useFocus from "hooks/useFocus";
+import { useContext } from "react";
+import { ThemeContext } from "styled-components/native";
 import * as S from "./styled";
 
 const OutlineInput = () => {
-  return <S.Input />;
+  const [inputProps, isFocus] = useFocus();
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <S.Input
+      {...inputProps}
+      {...{ isFocus }}
+      placeholder="hello wo"
+      placeholderTextColor={themeContext.colors.grayscale.scale30}
+    />
+  );
 };
 
 export default OutlineInput;
