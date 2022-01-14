9c402e9925c6f262fde9c63a61b0be4875bfaa4c
김진근 <freedom7113@gmail.com>
2022-01-14 14:24:44 +0900

    #8 Add add brand button

--- src/components/Buttons/Brand/index.tsx
+++ src/components/Buttons/Brand/index.tsx
@@ -1,1 +1,7 @@
-export {};
+import { Button } from "./styles";
+
+export const BrandButton = () => {
+  return <Button>hello world</Button>;
+};
+
+export default BrandButton;
--- src/components/Buttons/Brand/styles.ts
+++ src/components/Buttons/Brand/styles.ts
@@ -1,1 +1,8 @@
-export {};
+import styled from "styled-components";
+import { DefaultButtonStyle } from "../styles";
+
+export const Button = styled(DefaultButtonStyle)`
+  background-color: ${({ theme }) => theme.colors.primary.default};
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
+  border: 0;
+`;
