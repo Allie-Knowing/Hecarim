d429596332bda7e741cea24fdb076f5e5704bab6
김진근 <freedom7113@gmail.com>
2022-01-14 16:52:27 +0900

    #8 Modify Add Outline Destructive Button

--- src/components/Buttons/OutlineDestructive/index.tsx
+++ src/components/Buttons/OutlineDestructive/index.tsx
@@ -1,1 +1,18 @@
-export {};
+import { useContext } from "react";
+import { ThemeContext } from "styled-components/native";
+import { ButtonProps } from "..";
+import * as S from "./styles";
+
+export const OutlineDestructiveButton = ({ onPress, children }: ButtonProps) => {
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <>
+      <S.Container onPress={onPress} underlayColor={themeContext.colors.grayscale.scale20}>
+        <S.Label>{children}</S.Label>
+      </S.Container>
+    </>
+  );
+};
+
+export default OutlineDestructiveButton;
