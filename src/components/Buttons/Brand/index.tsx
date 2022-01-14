456e236f22b0e6256694ef938064f0ad0d62bd1c
김진근 <freedom7113@gmail.com>
2022-01-14 16:41:30 +0900

    #8 Modify add Props in Brand button

--- src/components/Buttons/Brand/index.tsx
+++ src/components/Buttons/Brand/index.tsx
@@ -1,15 +1,18 @@
 import { useContext } from "react";
 import { TouchableHighlight } from "react-native";
 import { ThemeContext } from "styled-components/native";
+import { ButtonProps } from "..";
 import * as S from "./styles";
 
-export const BrandButton = () => {
+export const BrandButton = ({ onPress, children }: ButtonProps) => {
   const themeContext = useContext(ThemeContext);
 
   return (
-    <S.Container onPress={() => {}} underlayColor={themeContext.colors.primary.click}>
-      <S.Label>Hello world</S.Label>
-    </S.Container>
+    <>
+      <S.Container onPress={onPress} underlayColor={themeContext.colors.primary.click}>
+        <S.Label>{children}</S.Label>
+      </S.Container>
+    </>
   );
 };
 
