4e07d8bc924b8b5e21ec7146407e70a21a7830bb
김진근 <freedom7113@gmail.com>
2022-01-14 14:24:11 +0900

    #8 Add add default button style

---
+++ src/components/Buttons/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const DefaultButtonStyle = styled.button`
+  font: ${({ theme }) => theme.fonts.body3};
+  border-radius: 1000px;
+  outline: none;
+  padding: 8px 16px;
+`;
