57a8d8764b96e992f4590d6e04615ed1639d4fda
김진근 <freedom7113@gmail.com>
2022-01-14 18:06:27 +0900

    #8 Modify realize useFocus hook

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -1,7 +1,23 @@
 import { useState } from "react";
 
-const useFocus = () => {
+type Dispatch = [
+  inputProps: { onFocus: () => void; onBlur: () => void },
+  isFocus: boolean,
+  setFocus: React.Dispatch<React.SetStateAction<boolean>>
+];
+
+const useFocus = (): Dispatch => {
   const [isFocus, setFocus] = useState<boolean>(false);
+
+  const onFocus = () => {
+    setFocus(true);
+  };
+
+  const onBlur = () => {
+    setFocus(false);
+  };
+
+  return [{ onFocus, onBlur }, isFocus, setFocus];
 };
 
 export default useFocus;
