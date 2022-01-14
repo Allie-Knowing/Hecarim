78869d785a492480c7cd355ae447d94bb9fbc795
김진근 <freedom7113@gmail.com>
2022-01-14 16:48:34 +0900

    #8 Modify Add Destructive Button

--- src/components/Buttons/Destructive/index.tsx
+++ src/components/Buttons/Destructive/index.tsx
@@ -1,1 +1,18 @@
-export {};
+import { useContext } from "react";
+import { ThemeContext } from "styled-components/native";
+import { ButtonProps } from "..";
+import * as S from "./styles";
+
+export const Destructive = ({ onPress, children }: ButtonProps) => {
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <>
+      <S.Container onPress={onPress} underlayColor={themeContext.colors.red.click}>
+        <S.Label>{children}</S.Label>
+      </S.Container>
+    </>
+  );
+};
+
+export default Destructive;
--- src/components/Buttons/Destructive/styles.ts
+++ src/components/Buttons/Destructive/styles.ts
@@ -1,1 +1,11 @@
-export {};
+import styled from "styled-components/native";
+import * as I from "../styles";
+
+export const Container = styled(I.DefaultButtonStyle)`
+  background-color: ${({ theme }) => theme.colors.red.default};
+  border: 0;
+`;
+
+export const Label = styled(I.DefaultLabelStyle)`
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
+`;
