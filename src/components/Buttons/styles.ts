2c770ba51463e2cac0a92f2d96a029fda77586ca
김진근 <freedom7113@gmail.com>
2022-01-14 16:17:48 +0900

    #8 Modify change Tag

--- src/components/Buttons/styles.ts
+++ src/components/Buttons/styles.ts
@@ -1,8 +1,11 @@
-import styled from "styled-components";
+import styled from "styled-components/native";
 
-export const DefaultButtonStyle = styled.button`
-  font: ${({ theme }) => theme.fonts.body3};
+export const DefaultButtonStyle = styled.TouchableHighlight`
   border-radius: 1000px;
   outline: none;
   padding: 8px 16px;
+`;
+
+export const DefaultLabelStyle = styled.Text`
+  font: ${({ theme }) => theme.fonts.body3};
 `;
