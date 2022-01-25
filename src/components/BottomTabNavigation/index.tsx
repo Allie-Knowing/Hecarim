496d0f711d0c8fe4430de3d7064ea767279151fa
김진근 <freedom7113@gmail.com>
2022-01-25 12:52:40 +0900

    #10 Modify add bottom tab navigation component

---
+++ src/components/BottomTabNavigation/index.tsx
@@ -0,0 +1,9 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigation = () => {
+  return <div>hello world</div>;
+};
+
+export default BottomTabNavigation;
