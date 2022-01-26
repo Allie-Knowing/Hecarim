ca2e6331125060635adac5151f6b6524b335cff9
김진근 <freedom7113@gmail.com>
2022-01-26 13:38:03 +0900

    #10 Modify use navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import BottomTabNavigation from "components/BottomTabNavigation";
+import { StatusBar } from "expo-status-bar";
+import React from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "theme/theme";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <BottomTabNavigation />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
