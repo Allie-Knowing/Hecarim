048a6be971e8afc788fcae84f2b8fab4e12603b5
김진근 <freedom7113@gmail.com>
2022-01-26 11:53:57 +0900

    #10 Modify add navigation buttons

--- src/components/BottomTabNavigation/index.tsx
+++ src/components/BottomTabNavigation/index.tsx
@@ -1,9 +1,58 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { LabelPosition } from "@react-navigation/bottom-tabs/lib/typescript/src/types";
+import React, { useContext } from "react";
+import { Image, Text } from "react-native";
+import { ThemeContext } from "styled-components/native";
+import Label from "./Label";
+
+const FeedIconDisable = require("../../assets/icons/navigation/feed/deactive.svg");
+const MyPageIconDisable = require("../../assets/icons/navigation/mypage/deactive.svg");
+const SearchIconDisable = require("../../assets/icons/navigation/search/deactive.svg");
+const WalletIconDisable = require("../../assets/icons/navigation/wallet/deactive.svg");
 
 const Tab = createBottomTabNavigator();
 
+const iconMap = new Map<string, any>()
+  .set("feed", FeedIconDisable)
+  .set("mypage", MyPageIconDisable)
+  .set("search", SearchIconDisable)
+  .set("wallet", WalletIconDisable);
+
 const BottomTabNavigation = () => {
-  return <div>hello world</div>;
+  const themeContext = useContext(ThemeContext);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let tabIcon = iconMap.get(route.name)!;
+
+          return (
+            <Image
+              source={tabIcon}
+              resizeMode="contain"
+              style={{
+                height: 30,
+                width: 30,
+                tintColor: !focused
+                  ? themeContext.colors.grayscale.scale30
+                  : themeContext.colors.primary.default,
+              }}
+            />
+          );
+        },
+      })}
+    >
+      <Tab.Screen
+        name="feed"
+        options={{ tabBarLabel: Label("피드") }}
+        component={() => <Text>feed</Text>}
+      />
+      <Tab.Screen name="mypage" component={() => <Text>mypage</Text>} />
+      <Tab.Screen name="search" component={() => <Text>search</Text>} />
+      <Tab.Screen name="wallet" component={() => <Text>wallet</Text>} />
+    </Tab.Navigator>
+  );
 };
 
 export default BottomTabNavigation;
--- src/components/BottomTabNavigation/styles.ts
+++ src/components/BottomTabNavigation/styles.ts
@@ -1,1 +1,7 @@
-export {};
+import styled from "styled-components/native";
+
+export const Label = styled.Text<{ focused: boolean }>`
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme, focused }) =>
+    focused ? theme.colors.primary.default : theme.colors.grayscale.scale30};
+`;
