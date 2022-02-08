65ef7ad24be2b645dbed949cd280755ec18b6ada
김진근 <freedom7113@gmail.com>
2022-02-08 20:52:15 +0900

    #14 Add title in content

--- src/components/Feed/Content/index.tsx
+++ src/components/Feed/Content/index.tsx
@@ -1,1 +1,37 @@
-export {};
+import { Text, View } from "react-native";
+import * as S from "./styles";
+
+const Test = require("../../../assets/feed_test.jpg");
+
+const Content = () => {
+  return (
+    <S.Container>
+      <S.Video source={Test} />
+      <S.Content>
+        <S.InfoContainer>
+          <S.TitleContainer>
+            <View>
+              <S.Q>Q.&nbsp;</S.Q>
+            </View>
+            <S.Title>제가 흥얼거리는 노래 제목 알려줭</S.Title>
+          </S.TitleContainer>
+          <S.TitleContainer>
+            <View>
+              <S.InvisiblePadding>Q.&nbsp;</S.InvisiblePadding>
+            </View>
+            <S.Description>제가 흥얼거리는 노래 제목 알려주세요 대충...</S.Description>
+          </S.TitleContainer>
+        </S.InfoContainer>
+        <View>
+          <S.Icons>
+            <Text>qwe</Text>
+            <Text>qqwe</Text>
+            <Text>qqwe123</Text>
+          </S.Icons>
+        </View>
+      </S.Content>
+    </S.Container>
+  );
+};
+
+export default Content;
--- src/components/Feed/Content/styles.ts
+++ src/components/Feed/Content/styles.ts
@@ -1,1 +1,69 @@
-export {};
+import { Image } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+`;
+
+export const Video = styled(Image)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+`;
+
+export const Content = styled.View`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  padding-bottom: 80px;
+  align-items: flex-end;
+  column-gap: 20px;
+  flex-direction: row;
+`;
+
+export const InfoContainer = styled.View`
+  flex: 1;
+  row-gap: 5px;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
+  font: ${({ theme }) => theme.fonts.body1};
+  align-self: flex-end;
+`;
+
+export const Q = styled.Text`
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
+  font: ${({ theme }) => theme.fonts.h2};
+`;
+
+export const Description = styled.Text`
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
+  font: ${({ theme }) => theme.fonts.description2};
+`;
+
+export const InvisiblePadding = styled.Text`
+  color: transparent;
+  font: ${({ theme }) => theme.fonts.h2};
+`;
+
+export const Icons = styled.View`
+  flex-direction: column;
+  row-gap: 20px;
+  justify-content: flex-end;
+`;
+
+export const IconContainer = styled.View``;
