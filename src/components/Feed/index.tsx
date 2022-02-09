768c22a31b0fc4ea4926128a9b6ffb05e399541d
김진근 <freedom7113@gmail.com>
2022-02-09 14:04:14 +0900

    #14 Modify calculate page

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -3,10 +3,22 @@ import { Text } from "react-native";
 import Content from "./Content";
 import * as S from "./styles";
 import { Dimensions } from "react-native";
+import { useEffect, useState } from "react";
 
 const { height } = Dimensions.get("screen");
 
 const Feed = () => {
+  const [page, setPage] = useState(0);
+
+  const onScroll = (e: any) => {
+    const newPage = Math.round(e.nativeEvent.contentOffset.y / height);
+    setPage(newPage);
+  };
+
+  useEffect(() => {
+    console.log(page);
+  }, [page]);
+
   return (
     <S.Container
       decelerationRate="fast"
@@ -15,7 +27,9 @@ const Feed = () => {
       contentContainerStyle={{ flexGrow: 1 }}
       snapToInterval={height}
       showsVerticalScrollIndicator={false}
+      onScroll={onScroll}
       data={[1, 2, 3, 4]}
+      keyExtractor={(_, index) => index.toString()}
       renderItem={() => <Content />}
     />
   );
