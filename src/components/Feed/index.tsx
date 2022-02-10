24cd0bf28e96c592bc0e9f12f4fa320474a60ae6
김진근 <freedom7113@gmail.com>
2022-02-10 13:11:57 +0900

    #14 Modify declare scroll event type

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,6 +1,6 @@
 import Content from "./Content";
 import * as S from "./styles";
-import { Dimensions } from "react-native";
+import { Dimensions, NativeSyntheticEvent, NativeScrollEvent } from "react-native";
 import { useEffect, useState } from "react";
 
 const { height } = Dimensions.get("screen");
@@ -8,7 +8,7 @@ const { height } = Dimensions.get("screen");
 const Feed = () => {
   const [page, setPage] = useState(0);
 
-  const onScroll = (e: any) => {
+  const onScroll = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
     const newPage = Math.round(e.nativeEvent.contentOffset.y / height);
     setPage(newPage);
   };
