13cea595536b35f460fe0bf96c4b4eb4c2a8cff4
KJG04 <freedom7113@gmail.com>
2022-02-13 13:42:57 +0900

    #16 Modify load font

--- App.tsx
+++ App.tsx
@@ -4,8 +4,20 @@ import { StatusBar } from "expo-status-bar";
 import React from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import theme from "theme/theme";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "SpoqaHanSansNeo-Bold": require("./src/assets/fonts/SpoqaHanSansNeo-Bold.ttf"),
+    "SpoqaHanSansNeo-Medium": require("./src/assets/fonts/SpoqaHanSansNeo-Medium.ttf"),
+    "SpoqaHanSansNeo-Regular": require("./src/assets/fonts/SpoqaHanSansNeo-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
