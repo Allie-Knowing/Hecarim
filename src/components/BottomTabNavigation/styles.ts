9e890cf95790c954931ed3b2b8ebaa05fa243f63
KJG04 <freedom7113@gmail.com>
2022-02-13 13:17:58 +0900

    #16 Modify change stlye

--- src/components/BottomTabNavigation/styles.ts
+++ src/components/BottomTabNavigation/styles.ts
@@ -5,11 +5,10 @@ export const Label = styled.Text<{ focused: boolean }>`
   font: ${({ theme }) => theme.fonts.description2};
   color: ${({ theme, focused }) =>
     focused ? theme.colors.primary.default : theme.colors.grayscale.scale30};
-  margin-top: 2px;
+  margin-top: 4px;
 `;
 
 export const Container = styled.View`
   flex-direction: column;
   align-items: center;
-  row-gap: 2px;
 `;
