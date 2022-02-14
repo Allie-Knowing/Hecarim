86967e1c6c82b0aa9afb9b04a9821f35291ad86c
KJG04 <freedom7113@gmail.com>
2022-02-14 17:29:49 +0900

    #14 Modify implement comment component

--- src/components/BottomSheets/Comments/index.tsx
+++ src/components/BottomSheets/Comments/index.tsx
@@ -1,1 +1,54 @@
-export {};
+import BottomSheet, {
+  BottomSheetFlatList,
+  BottomSheetBackdrop,
+} from "@gorhom/bottom-sheet";
+import { useState } from "react";
+import {
+  forwardRef,
+  useCallback,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
+import { Text, View } from "react-native";
+import DefaultBackDropComponent from "../DefaultBackdropComponent";
+
+export interface CommentBottomSheetRefProps {
+  open: () => void;
+}
+
+const CommentBottomSheet = forwardRef<CommentBottomSheetRefProps>((_, ref) => {
+  const sheetRef = useRef<BottomSheet>(null);
+
+  const handleOpenPress = useCallback(() => {
+    sheetRef.current?.snapToIndex(0);
+  }, []);
+
+  const handleClosePress = useCallback(() => {
+    sheetRef.current?.close();
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    open: handleOpenPress,
+  }));
+
+  useEffect(() => {
+    handleClosePress();
+  }, []);
+
+  return (
+    <BottomSheet
+      ref={sheetRef}
+      enablePanDownToClose
+      backdropComponent={DefaultBackDropComponent}
+      snapPoints={["60%"]}
+      index={-1}
+    >
+      <View>
+        <Text>hello world!</Text>
+      </View>
+    </BottomSheet>
+  );
+});
+
+export default CommentBottomSheet;
