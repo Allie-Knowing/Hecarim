7dca8bd5329d67f55abe52ea664d6ca24486441f
KJG04 <freedom7113@gmail.com>
2022-02-16 14:23:14 +0900

    #14 Modify change color

--- src/components/BottomSheets/Comments/index.tsx
+++ src/components/BottomSheets/Comments/index.tsx
@@ -1,6 +1,7 @@
-import { forwardRef } from "react";
+import { forwardRef, useContext } from "react";
 import { Dimensions, Text, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
+import { ThemeContext } from "styled-components/native";
 
 const { height } = Dimensions.get("screen");
 export interface CommentBottomSheetRefProps {
@@ -8,6 +9,8 @@ export interface CommentBottomSheetRefProps {
 }
 
 const CommentBottomSheet = forwardRef<RBSheet>((_, ref) => {
+  const themeContext = useContext(ThemeContext);
+
   return (
     <RBSheet
       ref={ref}
@@ -15,6 +18,14 @@ const CommentBottomSheet = forwardRef<RBSheet>((_, ref) => {
       openDuration={400}
       closeOnDragDown
       closeOnPressMask
+      customStyles={{
+        container: {
+          backgroundColor: themeContext.colors.grayscale.scale100,
+        },
+        draggableIcon: {
+          backgroundColor: themeContext.colors.grayscale.scale70,
+        },
+      }}
     >
       <View>
         <Text>hello world!</Text>
