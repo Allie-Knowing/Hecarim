825de5230f650a08a7a8ecaa058bfeb5ad760ba4
KJG04 <freedom7113@gmail.com>
2022-02-19 01:04:25 +0900

    #19 Add feed screen

---
+++ src/screens/Feed/index.tsx
@@ -0,0 +1,29 @@
+import { Dimensions } from "react-native";
+import * as S from "./styles";
+import FeedVideos from "../../components/Feed";
+import VideoAnswer from "components/VideoAnswer";
+import React, { FC } from "react";
+
+const { height, width } = Dimensions.get("screen");
+
+const Feed = () => {
+  return (
+    <S.Wrapper style={{ height }}>
+      <S.Outer
+        decelerationRate="fast"
+        snapToAlignment="start"
+        pagingEnabled
+        snapToInterval={width}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyExtractor={(_, index) => index.toString()}
+        data={[FeedVideos, VideoAnswer]}
+        renderItem={(value) => React.createElement(value.item as FC)}
+      />
+    </S.Wrapper>
+  );
+};
+
+export default Feed;
---
+++ src/screens/Feed/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components/native";
+
+export const Outer = styled.FlatList`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+`;
+
+export const Wrapper = styled.View`
+  position: absolute;
+  width: 100%;
+  top: 0px;
+  left: 0px;
+`;
