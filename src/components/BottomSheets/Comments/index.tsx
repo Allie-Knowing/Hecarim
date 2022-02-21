00567d60584966ba1a90ff661f227a32f7cded24
KJG04 <freedom7113@gmail.com>
2022-02-21 16:11:20 +0900

    #19 add input action

--- src/components/BottomSheets/Comments/index.tsx
+++ src/components/BottomSheets/Comments/index.tsx
@@ -1,12 +1,14 @@
 import Comment from "components/Comment";
-import { forwardRef, useContext } from "react";
+import { forwardRef, useContext, useState } from "react";
 import { Dimensions, Text, TouchableOpacity, View } from "react-native";
 import { Portal } from "react-native-portalize";
 import RBSheet from "react-native-raw-bottom-sheet";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ThemeContext } from "styled-components/native";
-import BottomSheet from "@gorhom/bottom-sheet";
+import BottomSheet, { BottomSheetTextInput } from "@gorhom/bottom-sheet";
 import * as S from "./styles";
+import DefaultBackDropComponent from "../DefaultBackdropComponent";
+import useFocus from "hooks/useFocus";
 
 const { height } = Dimensions.get("screen");
 export interface CommentBottomSheetRefProps {
@@ -18,16 +20,50 @@ const TestImage = require("../../../assets/feed_test.jpg");
 const CommentBottomSheet = forwardRef<BottomSheet>((_, ref) => {
   const themeContext = useContext(ThemeContext);
   const { bottom: bottomPad } = useSafeAreaInsets();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [inputProps, isFocus] = useFocus();
 
   return (
     <Portal>
       <BottomSheet
         ref={ref}
-        snapPoints={["60%"]}
+        snapPoints={["70%"]}
         enablePanDownToClose
+        enableOverDrag
         index={-1}
+        backdropComponent={DefaultBackDropComponent(isOpen)}
+        handleStyle={{
+          backgroundColor: themeContext.colors.grayscale.scale100,
+          borderTopRightRadius: 10,
+          borderTopLeftRadius: 10,
+        }}
+        handleIndicatorStyle={{
+          backgroundColor: themeContext.colors.grayscale.scale50,
+        }}
+        onChange={(index) => setIsOpen(index !== -1)}
+        keyboardBehavior="extend"
       >
-        <Text>hello world!</Text>
+        <S.Container>
+          <S.Title>댓글</S.Title>
+          <S.List
+            data={[1, 2, 3, 4, 5, 6, 7]}
+            keyExtractor={(i) => `comment_${i}`}
+            renderItem={() => <Comment />}
+            showsVerticalScrollIndicator={false}
+          />
+        </S.Container>
+        <S.InputContainer>
+          <S.InputProfile source={TestImage} />
+          <S.Input
+            placeholder="KJG04로 답변 추가"
+            placeholderTextColor={themeContext.colors.grayscale.scale30}
+            {...inputProps}
+          />
+          <TouchableOpacity>
+            <S.Submit>추가</S.Submit>
+          </TouchableOpacity>
+        </S.InputContainer>
+        <S.InputMargin style={{ height: isFocus ? 0 : bottomPad }} />
       </BottomSheet>
     </Portal>
   );
--- src/components/BottomSheets/Comments/styles.ts
+++ src/components/BottomSheets/Comments/styles.ts
@@ -1,55 +1,51 @@
+import {
+  BottomSheetFlatList,
+  BottomSheetTextInput,
+} from "@gorhom/bottom-sheet";
 import styled from "styled-components/native";
 
-export const Title = styled.Text`
-  font: ${({ theme }) => theme.fonts.body2};
-  color: ${({ theme }) => theme.colors.grayscale.scale10};
-  margin-bottom: 8px;
-`;
-
 export const Container = styled.View`
-  padding: 10px 20px;
-  padding-bottom: 0px;
-  height: 83%;
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.grayscale.scale100};
+  padding: 10px 20px 0px 20px;
 `;
 
-export const Scroll = styled.ScrollView``;
-
-export const ScrollInner = styled.TouchableWithoutFeedback``;
-
-export const Input = styled.TextInput`
-  flex: 1;
+export const Title = styled.Text`
+  font: ${({ theme }) => theme.fonts.subtitle2};
   color: ${({ theme }) => theme.colors.grayscale.scale10};
-  font: ${({ theme }) => theme.fonts.description1};
+  padding-bottom: 4px;
+`;
+
+export const List = styled(BottomSheetFlatList)`
+  flex: 1;
 `;
 
 export const InputContainer = styled.View`
   background-color: ${({ theme }) => theme.colors.grayscale.scale90};
-  padding-top: 5px;
-  padding-right: 20px;
-  padding-left: 20px;
-  height: 15%;
-`;
-
-export const InputInner = styled.View`
+  padding: 10px 20px;
   flex-direction: row;
   align-items: center;
-  flex: 1;
 `;
 
-export const InputWrapper = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+export const InputMargin = styled.View`
+  background-color: ${({ theme }) => theme.colors.grayscale.scale90};
 `;
 
-export const ProfileImage = styled.Image`
-  width: 30px;
-  height: 30px;
+export const InputProfile = styled.Image`
+  width: 36px;
+  height: 36px;
   border-radius: 1000px;
-  margin-right: 10px;
+  margin-right: 16px;
 `;
 
 export const Submit = styled.Text`
-  font: ${({ theme }) => theme.fonts.description1};
+  font: ${({ theme }) => theme.fonts.body3};
   color: ${({ theme }) => theme.colors.primary.default};
+  margin-left: 16px;
+`;
+
+export const Input = styled(BottomSheetTextInput)`
+  flex: 1;
+  font: ${({ theme }) => theme.fonts.body3};
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
 `;
