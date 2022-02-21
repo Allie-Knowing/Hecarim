09a595dc40bf456e7fd8f272ff2054916814881d
KJG04 <freedom7113@gmail.com>
2022-02-21 14:43:52 +0900

    #19 Modify add portal host

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigation from "components/BottomTabNavigation";
-import { StatusBar } from "expo-status-bar";
 import React from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import theme from "theme/theme";
