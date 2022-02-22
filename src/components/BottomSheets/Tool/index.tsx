51c6859553a1df6b9f0646d757cb7b4275d8a27b
KJG04 <freedom7113@gmail.com>
2022-02-22 17:21:24 +0900

    #21 Modify tool sheet auto height

--- src/components/BottomSheets/Tool/index.tsx
+++ src/components/BottomSheets/Tool/index.tsx
@@ -1,7 +1,9 @@
-import BottomSheet from "@gorhom/bottom-sheet";
+import BottomSheet, {
+  BottomSheetView,
+  useBottomSheetDynamicSnapPoints,
+} from "@gorhom/bottom-sheet";
 import { forwardRef, useContext, useState } from "react";
 import { View } from "react-native";
-import { Portal } from "react-native-portalize";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { ThemeContext } from "styled-components/native";
 import DefaultBackDropComponent from "../DefaultBackdropComponent";
@@ -21,25 +23,34 @@ const Tool = forwardRef<BottomSheet, PropsType>(({ items }, ref) => {
   const { bottom: bottomPad } = useSafeAreaInsets();
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const themeContext = useContext(ThemeContext);
+  const {
+    animatedSnapPoints,
+    animatedContentHeight,
+    animatedHandleHeight,
+    handleContentLayout,
+  } = useBottomSheetDynamicSnapPoints(["CONTENT_HEIGHT"]);
 
   return (
-    <Portal>
-      <BottomSheet
-        ref={ref}
-        snapPoints={["30%"]}
-        index={-1}
-        onChange={(index) => setIsOpen(index !== -1)}
-        backdropComponent={DefaultBackDropComponent(isOpen)}
-        enablePanDownToClose
-        handleStyle={{
-          backgroundColor: themeContext.colors.grayscale.scale20,
-          borderTopLeftRadius: 10,
-          borderTopRightRadius: 10,
-        }}
-        handleIndicatorStyle={{
-          backgroundColor: themeContext.colors.grayscale.scale50,
-        }}
-      >
+    <BottomSheet
+      ref={ref}
+      snapPoints={animatedSnapPoints}
+      handleHeight={animatedHandleHeight}
+      contentHeight={animatedContentHeight}
+      index={-1}
+      onChange={(index) => setIsOpen(index !== -1)}
+      backdropComponent={DefaultBackDropComponent(isOpen)}
+      enablePanDownToClose
+      animateOnMount
+      handleStyle={{
+        backgroundColor: themeContext.colors.grayscale.scale20,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+      }}
+      handleIndicatorStyle={{
+        backgroundColor: themeContext.colors.grayscale.scale50,
+      }}
+    >
+      <BottomSheetView onLayout={handleContentLayout}>
         <S.Container>
           {items.map((value, index) => (
             <S.Button
@@ -53,8 +64,8 @@ const Tool = forwardRef<BottomSheet, PropsType>(({ items }, ref) => {
           ))}
           <S.Pad style={{ height: bottomPad }} />
         </S.Container>
-      </BottomSheet>
-    </Portal>
+      </BottomSheetView>
+    </BottomSheet>
   );
 });
 
