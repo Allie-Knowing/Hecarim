bad94321833ec821cb49188566251341089b45a5
KJG04 <freedom7113@gmail.com>
2022-03-31 23:27:39 +0900

    #48 Modify refresh token logic using axios interceptor

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import env from "constant/env";
 import { refresh } from "./refresh";
 
@@ -6,15 +6,15 @@ export const instance = axios.create({ baseURL: env.baseUrl });
 
 export const noTokenInstance = axios.create({ baseURL: env.baseUrl });
 
-instance.interceptors.request.use(refresh, function (error: AxiosError) {
-  return Promise.reject(error);
-});
-
-instance.interceptors.response.use(
-  function (response) {
-    return response;
+instance.interceptors.request.use(
+  function (config) {
+    return config;
   },
   function (error: AxiosError) {
     return Promise.reject(error);
   }
 );
+
+instance.interceptors.response.use(function (response) {
+  return response;
+}, refresh);
--- src/utils/axios/refresh.ts
+++ src/utils/axios/refresh.ts
@@ -1,1 +1,37 @@
-export const refresh = () => {};
+import axios, { AxiosError } from "axios";
+import storageKeys from "constant/storageKeys";
+import localStorage from "utils/localStorage";
+import { noTokenInstance } from ".";
+
+interface RefreshResponse {
+  access_token: string;
+  refresh_token: string;
+}
+
+export const refresh = async (error: AxiosError) => {
+  if (error.response.status !== 401) {
+    //토큰 오류가 아니면 그냥 에러 발생
+    return Promise.reject(error);
+  }
+
+  //리프레시 로직
+  const refreshToken = await localStorage.getItem<string>(
+    storageKeys.refreshToken
+  );
+  await localStorage.removeItem(storageKeys.accessToken);
+
+  const { access_token, refresh_token } = (
+    await noTokenInstance.post<RefreshResponse>("/auth/refresh", {
+      refresh_token: refreshToken,
+    })
+  ).data;
+
+  await Promise.all([
+    localStorage.setItem(storageKeys.accessToken, access_token),
+    localStorage.setItem(storageKeys.refreshToken, refresh_token),
+  ]);
+
+  error.config.headers["Authorization"] = `Bearer ${access_token}`;
+
+  return axios(error.config);
+};
