6faae1acca96c613eb29636d53ac290227525748
Jeon gyuhyun <jgh10070@naver.com>
2022-03-31 17:15:22 +0900

    #27 Update SearchPage -all

--- src/components/SearchPage/SearchTopNavigation/InputNavigation/style.ts
+++ src/components/SearchPage/SearchTopNavigation/InputNavigation/style.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components/native";
 
-export const Wrapper = styled.View`
+export const Wrapper = styled.View<{ topPad:number }>`
+    justify-content: center;
+    align-items: center;
+    border: 1px ${({theme}) => theme.colors.grayscale.scale20};
+    background-color: ${({theme}) => theme.colors.grayscale.scale20};
+    border-radius: 10;
+    height: 50;
+    width: 340;
+`;
 
+export const MagnifyImage = styled.Image`
+    position: absolute;
+    left: 8px;
+    width: 18px;
+    height: 18px;
+    z-index: 2;
+`;
+
+export const Input = styled.TextInput<{ topPad: number }>`
+    position: absolute;
+    padding: 0 32px;
+    width: 100%;
+    height: 100%;
+    color: ${({theme}) => theme.colors.grayscale.scale50};
+    font: ${({theme}) => theme.fonts.body3};
 `;
