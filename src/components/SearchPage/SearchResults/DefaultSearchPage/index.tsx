d6623520aa945beb6d7fe944c4cbe5e135766bf2
Jeon gyuhyun <jgh10070@naver.com>
2022-04-01 12:00:01 +0900

    #27 Modify default search page structure

--- src/components/SearchPage/SearchResults/DefaultSearchPage/index.tsx
+++ src/components/SearchPage/SearchResults/DefaultSearchPage/index.tsx
@@ -1,21 +1,29 @@
-import SearchTopNavigation from "components/SearchPage/SearchTopNavigation";
 import React, { FC } from "react";
-import { Fragment } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "screens/Search";
 import * as S from "./style";
+import SearchTopNavigation from "components/SearchPage/SearchTopNavigation";
 
 const { height } = Dimensions.get("screen");
 
+type screenProp = StackNavigationProp<RootStackParamList, "SearchedQuestions">;
+
 const DefaultSearchPage: FC = () => {
+  const navigation = useNavigation<screenProp>();
+
   return (
-    <Fragment>
+    <S.Wrapper>
       <SearchTopNavigation />
-      <S.Wrapper height={height / 1.374}>
-        <S.View>
-          <S.Text>검색을 통해 질문을 찾아보세요.</S.Text>
-        </S.View>
-      </S.Wrapper>
-    </Fragment>
+      <S.View height={height / 1.44}>
+        <S.Text>검색을 통해 질문을 찾아보세요.</S.Text>
+        <Button
+          title="Go"
+          onPress={() => navigation.navigate("SearchedQuestions")}
+        />
+      </S.View>
+    </S.Wrapper>
   );
 };
 
