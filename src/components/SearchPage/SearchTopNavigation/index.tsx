8f558613733d51ca70bb555d7fde2cc95a6794dc
Jeon gyuhyun <jgh10070@naver.com>
2022-04-01 11:56:49 +0900

    #27 Modify delete component (title)

--- src/components/SearchPage/SearchTopNavigation/index.tsx
+++ src/components/SearchPage/SearchTopNavigation/index.tsx
@@ -2,14 +2,12 @@ import React, { FC } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as S from "./style";
 import InputNavigation from "./InputNavigation";
-import TitleNavigation from "./TitleNavigation";
 
 const SearchTopNavigation: FC = () => {
   const { top: topPad } = useSafeAreaInsets();
 
   return (
     <S.Wrapper topPad={topPad}>
-      {/* <TitleNavigation topPad={topPad} /> */}
       <InputNavigation topPad={topPad} />
     </S.Wrapper>
   );
