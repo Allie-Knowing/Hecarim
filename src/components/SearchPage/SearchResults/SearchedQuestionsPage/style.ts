fb17911d9a824ba652ed7d38643bee92909ad5b2
Jeon gyuhyun <jgh10070@naver.com>
2022-04-01 11:57:40 +0900

    #27 Modify serached question page styled

--- src/components/SearchPage/SearchResults/SearchedQuestionsPage/style.ts
+++ src/components/SearchPage/SearchResults/SearchedQuestionsPage/style.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components/native";
+import Animated from "react-native-reanimated";
 
 export const Wrapper = styled.View`
+    position: relative;
+`;
+
+export const BackBtn = styled.Image<{ topPad: number }>`
     position: absolute;
+    width: 10;
+    height: 18;
+    z-index: 10;
+`;
+
+export const View = styled(Animated.View)<{ height: number }>`
+    height: ${({height}) => height};
 `;
