dc7238fcdce609b77f25de1dc77b7922ead00383
Jeon gyuhyun <jgh10070@naver.com>
2022-04-01 11:57:53 +0900

    #27 Modify searched question page structure

--- src/components/SearchPage/SearchResults/SearchedQuestionsPage/index.tsx
+++ src/components/SearchPage/SearchResults/SearchedQuestionsPage/index.tsx
@@ -1,11 +1,26 @@
 import React, { FC } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Image } from "react-native";
+import Animated from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import SearchTopNavigation from "components/SearchPage/SearchTopNavigation";
 import * as S from "./style";
 
 const { height } = Dimensions.get("screen");
 
 const SearchedQuestionsPage: FC = () => {
-  return <S.Wrapper></S.Wrapper>;
+  const { top: topPad } = useSafeAreaInsets();
+
+  return (
+    <S.Wrapper>
+      {/* <S.BackBtn
+        topPad={topPad}
+        source={require("../../../../assets/icons/Search/Back_btn.png")}
+        style={{ resizeMode: "stretch" }}
+      /> */}
+      <SearchTopNavigation />
+      <S.View height={height}></S.View>
+    </S.Wrapper>
+  );
 };
 
 export default SearchedQuestionsPage;
