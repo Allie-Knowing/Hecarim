d095b86c6a44e0390790215ed2e6c5981f28fc41
Jeon gyuhyun <jgh10070@naver.com>
2022-04-01 11:56:06 +0900

    #27 Modify input navigation

--- src/components/SearchPage/SearchTopNavigation/InputNavigation/style.ts
+++ src/components/SearchPage/SearchTopNavigation/InputNavigation/style.ts
@@ -7,12 +7,12 @@ export const Wrapper = styled.View<{ topPad:number }>`
     background-color: ${({theme}) => theme.colors.grayscale.scale20};
     border-radius: 10;
     height: 50;
-    width: 340;
+    width: 365;
 `;
 
 export const MagnifyImage = styled.Image`
     position: absolute;
-    left: 8px;
+    left: 8;
     width: 18px;
     height: 18px;
     z-index: 2;
@@ -23,6 +23,17 @@ export const Input = styled.TextInput<{ topPad: number }>`
     padding: 0 32px;
     width: 100%;
     height: 100%;
-    color: ${({theme}) => theme.colors.grayscale.scale50};
+    color: ${({theme}) => theme.colors.grayscale.scale100};
     font: ${({theme}) => theme.fonts.body3};
+`;
+
+export const ResetImageContainer = styled.TouchableOpacity`
+    position: absolute;
+    right: 8;
+`;
+
+export const ResetTextImage = styled.Image`
+    width: 18;
+    height: 18;
+    z-index: 2;
 `;
