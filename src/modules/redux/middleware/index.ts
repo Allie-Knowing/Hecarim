882c0ff4665e370cc38454a9b303118eef6d489a
Kimjimin0807 <jm004471@naver.com>
2022-04-02 14:59:45 +0900

    #31 Add oauth Saga

--- src/modules/redux/middleware/index.ts
+++ src/modules/redux/middleware/index.ts
@@ -1,5 +1,6 @@
-import { all } from 'redux-saga/effects';
+import { all } from "redux-saga/effects";
+import signinSaga from "./signin";
 
 export default function* rootSaga() {
-    yield all([]);
+  yield all([signinSaga()]);
 }
