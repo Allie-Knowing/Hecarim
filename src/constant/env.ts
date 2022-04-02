a4b2ea0a94b78d2e5ac273098fff70d37c2f4b40
Jeon gyuhyun <jgh10070@naver.com>
2022-04-02 15:01:51 +0900

    Modify env.ts in constant

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,10 +1,25 @@
 import getEnvVars from "../../environment";
 
-const { baseUrl, clientId } = getEnvVars();
+const {
+  baseUrl,
+  googleIosClientId,
+  googleWebClientId,
+  naverClientId,
+  facebookClientId,
+  kakaoClientId,
+  redirectUrl,
+} = getEnvVars();
 
 const env = {
-  baseUrl: baseUrl,
-  clientId: clientId,
+  baseUrl,
+  googleClientId: {
+    iosId: googleIosClientId,
+    webId: googleWebClientId,
+  },
+  naverClientId,
+  facebookClientId,
+  kakaoClientId,
+  redirectUrl,
 } as const;
 
 export default env;
