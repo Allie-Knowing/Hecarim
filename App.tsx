0e6f28a043370ccec2eba43214b4f41cc78b9e03
Jeon gyuhyun <jgh10070@naver.com>
2022-04-02 15:00:05 +0900

    Modify Set Provider [stroe] in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import React from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigation from "components/BottomTabNavigation";
-import React from "react-native";
 import { ThemeProvider } from "styled-components/native";
-import theme from "theme/theme";
+import { Provider } from "react-redux";
 import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import store from "modules/redux/store";
+import theme from "theme/theme";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -23,11 +25,13 @@ export default function App() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }} edges={["left", "right"]}>
         <ThemeProvider theme={theme}>
-          <BottomSheetModalProvider>
-            <NavigationContainer>
-              <BottomTabNavigation />
-            </NavigationContainer>
-          </BottomSheetModalProvider>
+          <Provider store={store}>
+            <BottomSheetModalProvider>
+              <NavigationContainer>
+                <BottomTabNavigation />
+              </NavigationContainer>
+            </BottomSheetModalProvider>
+          </Provider>
         </ThemeProvider>
       </SafeAreaView>
     </SafeAreaProvider>
