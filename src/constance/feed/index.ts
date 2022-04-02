bde799aa634e88b2ab1d7f694d21b023df8db540
Jeon gyuhyun <jgh10070@naver.com>
2022-04-02 15:02:51 +0900

    Set constance /feed

---
+++ src/constance/feed/index.ts
@@ -0,0 +1,10 @@
+export type getVideoAnswerListType = {
+    videoId: number,
+    title: string,
+    userId: number,
+    profile: string,
+    videoUrl: string,
+    createdAt: string,
+    likeCnt: string,
+    isAdoption: number,
+} 
