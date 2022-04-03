32879f8acb26a86c131abcb6729d0e99e054311a
KJG04 <freedom7113@gmail.com>
2022-04-03 20:01:52 +0900

    #68 Modify text answer action type

--- src/modules/redux/action/textAnswer/index.ts
+++ src/modules/redux/action/textAnswer/index.ts
@@ -1,5 +1,7 @@
 import { error } from "modules/dto/error";
-import { createAction } from "typesafe-actions/dist/deprecated/create-action";
+import { deleteTextAnswerRequest, getTextAnswerRequest, postTextAnswerRequest } from "modules/dto/request/textAnswerRequest";
+import { getTextAnswerResponse } from "modules/dto/response/textAnswerResponse";
+import { createAction } from "typesafe-actions";
 import {
   DELETE_TEXT_ANSWER,
   DELETE_TEXT_ANSWER_FAILURE,
@@ -12,14 +14,25 @@ import {
   POST_TEXT_ANSWER_SUCCESS,
 } from "./interface";
 
-export const getTextAnswer = createAction(GET_TEXT_ANSWER)();
-export const getTextAnswerSuccess = createAction(GET_TEXT_ANSWER_SUCCESS)();
+export const getTextAnswer = createAction(GET_TEXT_ANSWER)<getTextAnswerRequest>();
+export const getTextAnswerSuccess = createAction(GET_TEXT_ANSWER_SUCCESS)<getTextAnswerResponse>();
 export const getTextAnswerFailure = createAction(GET_TEXT_ANSWER_FAILURE)<error>();
 
-export const postTextAnswer = createAction(POST_TEXT_ANSWER)();
+export const postTextAnswer = createAction(POST_TEXT_ANSWER)<postTextAnswerRequest>();
 export const postTextAnswerSuccess = createAction(POST_TEXT_ANSWER_SUCCESS)();
 export const postTextAnswerFailure = createAction(POST_TEXT_ANSWER_FAILURE)<error>();
 
-export const deleteTextAnswer = createAction(DELETE_TEXT_ANSWER)();
+export const deleteTextAnswer = createAction(DELETE_TEXT_ANSWER)<deleteTextAnswerRequest>();
 export const deleteTextAnswerSuccess = createAction(DELETE_TEXT_ANSWER_SUCCESS)();
 export const deleteTextAnswerFailure = createAction(DELETE_TEXT_ANSWER_FAILURE)<error>();
+
+export type textAnswerActionType =
+  | ReturnType<typeof getTextAnswer>
+  | ReturnType<typeof getTextAnswerSuccess>
+  | ReturnType<typeof getTextAnswerFailure>
+  | ReturnType<typeof postTextAnswer>
+  | ReturnType<typeof postTextAnswerSuccess>
+  | ReturnType<typeof postTextAnswerFailure>
+  | ReturnType<typeof deleteTextAnswer>
+  | ReturnType<typeof deleteTextAnswerSuccess>
+  | ReturnType<typeof deleteTextAnswerFailure>;
