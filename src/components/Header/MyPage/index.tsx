8ade9acc8e20cc7dc1d55be91412c239dae3b42b
Kimjimin0807 <jm004471@naver.com>
2022-04-03 21:41:43 +0900

    #62 Add settingPage

--- src/components/Header/MyPage/index.tsx
+++ src/components/Header/MyPage/index.tsx
@@ -1,13 +1,30 @@
+import { StackNavigationProp } from "@react-navigation/stack";
+import { MainStackParamList } from "hooks/useMainStackNavigation";
 import React, { FC } from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as S from "./style";
 
-const MyPageHeader: FC = () => {
+const setting = require("assets/icons/MyPage/setting.png");
+
+type Props = {
+  stackNavigation: StackNavigationProp<MainStackParamList, "UserPage">;
+};
+
+const MyPageHeader: FC<Props> = ({ stackNavigation }) => {
   const { top: topPad } = useSafeAreaInsets();
 
   return (
     <S.Container topPad={topPad}>
+      <S.Box />
       <S.Title topPad={topPad}>마이페이지</S.Title>
+      <TouchableOpacity
+        onPress={() => {
+          stackNavigation.push("Setting");
+        }}
+      >
+        <S.Setting source={setting} topPad={topPad} />
+      </TouchableOpacity>
     </S.Container>
   );
 };
--- src/components/Header/MyPage/style.ts
+++ src/components/Header/MyPage/style.ts
@@ -6,9 +6,14 @@ export const Container = styled.View<{
   width: 100%;
   position: relative;
   height: ${({ topPad }) => 50 + topPad}px;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.grayscale.scale10};
+`;
+
+export const Box = styled.View`
+  width: 40px;
 `;
 
 export const Title = styled.Text<{
@@ -17,4 +22,11 @@ export const Title = styled.Text<{
   color: ${({ theme }) => theme.colors.grayscale.scale100};
   font: ${({ theme }) => theme.fonts.body2};
   margin-top: ${({ topPad }) => topPad}px;
+`;
+
+export const Setting = styled.Image<{ topPad: number }>`
+  width: 20px;
+  height: 20px;
+  margin-top: ${({ topPad }) => topPad}px;
+  margin-right: 20px;
 `;
