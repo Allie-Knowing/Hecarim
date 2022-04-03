f34363d9231b78641532786323ed27d740e1086f
KJG04 <freedom7113@gmail.com>
2022-04-03 17:05:21 +0900

    #45 Modify add like saga and like api

--- src/utils/api/feed/index.ts
+++ src/utils/api/feed/index.ts
@@ -1,15 +1,8 @@
-// import { instance } from "utils/axios";
-import axios from "axios";
-import { getRequest, getRequestWithAccessToken } from "../default";
+import { getRequest } from "../default";
 import uri from "constance/uri";
 
 export const getVideoAnswerListApi = async () => {
-  // eslint-disable-next-line no-useless-catch
-  try {
-    const request = getRequest();
-    const data = await request.get(uri.video_answer);
-    return data;
-  } catch (error) {
-    throw error;
-  }
-}
+  const request = getRequest();
+  const data = await request.get(uri.video_answer);
+  return data;
+};
