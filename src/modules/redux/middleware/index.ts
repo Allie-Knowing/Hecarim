8038669270a513a8b9d3e8cadb4ac49c41008cb4
KJG04 <freedom7113@gmail.com>
2022-04-03 20:09:46 +0900

    #68 Add text answer saga

--- src/modules/redux/middleware/index.ts
+++ src/modules/redux/middleware/index.ts
@@ -2,7 +2,8 @@ import { all } from "redux-saga/effects";
 import feedSaga from "./feed";
 import likeSaga from "./like";
 import signinSaga from "./signin";
+import textAnswerSaga from "./textAnswer";
 
 export default function* rootSaga() {
-  yield all([feedSaga(), signinSaga(), likeSaga()]);
+  yield all([feedSaga(), signinSaga(), likeSaga(), textAnswerSaga()]);
 }
--- src/modules/redux/reducer/index.ts
+++ src/modules/redux/reducer/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import feedReducer from "./feed";
 import likeReducer from "./like";
 import signinReducer from "./signin";
+import textAnswerReducer from "./textAnswer";
 
 const rootReducer = combineReducers({
   feed: feedReducer,
   signin: signinReducer,
   like: likeReducer,
+  textAnswer: textAnswerReducer,
 });
 
 export type reducerType = ReturnType<typeof rootReducer>;
