66c641a5f3128589265584933d1db7c2d1c84bf6
Kimjimin0807 <jm004471@naver.com>
2022-04-04 22:03:53 +0900

    #62 Fix code

--- src/modules/redux/middleware/myId/index.ts
+++ src/modules/redux/middleware/myId/index.ts
@@ -1,4 +1,4 @@
-import { GET_MY_ID } from "modules/redux/action/myId/interface";\
+import { GET_MY_ID } from "modules/redux/action/myId/interface";
 import { takeLatest } from "redux-saga/effects";
 import { getMyId } from "utils/api/myId";
 import createRequestSaga from "utils/saga/createRequestSaga";
