3a898e1cdf076a50fa4084fd91f1652ca7148359
Kimjimin0807 <jm004471@naver.com>
2022-04-05 01:55:42 +0900

    #62 Add getProfileQuestionListSaga

--- src/modules/redux/middleware/profile/index.ts
+++ src/modules/redux/middleware/profile/index.ts
@@ -1,12 +1,20 @@
-import { GET_PROFILE } from "modules/redux/action/profile/interface";
+import {
+  GET_PROFILE,
+  GET_PROFILE_QUESTION_LIST,
+} from "modules/redux/action/profile/interface";
 import { takeLatest } from "redux-saga/effects";
-import { getProfile } from "utils/api/profile";
+import { getProfile, getProfilQuestionList } from "utils/api/profile";
 import createRequestSaga from "utils/saga/createRequestSaga";
 
 export const getProfileSaga = createRequestSaga(GET_PROFILE, getProfile);
+export const getProfileQuestionListSage = createRequestSaga(
+  GET_PROFILE_QUESTION_LIST,
+  getProfilQuestionList
+);
 
 function* profileSaga() {
   yield takeLatest(GET_PROFILE, getProfileSaga);
+  yield takeLatest(GET_PROFILE_QUESTION_LIST, getProfileQuestionListSage);
 }
 
 export default profileSaga;
