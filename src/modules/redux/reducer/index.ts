fc838b808d8c03301d8e650d7c8a0f327f8a4492
Jun Joy <chojuneseo1@gmail.com>
2022-04-04 20:27:07 +0900

    #58 Add add camera reducer to root reducer

--- src/modules/redux/reducer/index.ts
+++ src/modules/redux/reducer/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import feedReducer from "./feed";
 import likeReducer from "./like";
 import signinReducer from "./signin";
+import cameraReducer from "./camera";
 
 const rootReducer = combineReducers({
   feed: feedReducer,
   signin: signinReducer,
   like: likeReducer,
+  camera: cameraReducer,
 });
 
 export type reducerType = ReturnType<typeof rootReducer>;
