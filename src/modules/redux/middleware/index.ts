177fe0c9596aa4e58c397b72d3d127d0f64bb5eb
Jun Joy <chojuneseo1@gmail.com>
2022-04-04 22:51:53 +0900

    #4 Fix gyu bug

--- src/modules/redux/middleware/index.ts
+++ src/modules/redux/middleware/index.ts
@@ -1,12 +1,12 @@
 import { all } from "redux-saga/effects";
-import feedSaga from "./feed";
 import likeSaga from "./like";
 import myIdSaga from "./myId";
 import profileSaga from "./profile";
 import signinSaga from "./signin";
 import textAnswerSaga from "./textAnswer";
 import cameraSaga from "./camera";
+import answer from "./answer";
 
 export default function* rootSaga() {
-  yield all([feedSaga(), signinSaga(), likeSaga(), profileSaga(), myIdSaga(), textAnswerSaga(), cameraSaga()]);
+  yield all([answer(), signinSaga(), likeSaga(), profileSaga(), myIdSaga(), textAnswerSaga(), cameraSaga()]);
 }
