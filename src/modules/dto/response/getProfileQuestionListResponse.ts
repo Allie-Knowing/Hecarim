b2e20a3fca31f227d1fde261ea26285847dba2dd
Kimjimin0807 <jm004471@naver.com>
2022-04-05 01:54:42 +0900

    #62 Fix getProfileQuestionList dto

--- src/modules/dto/response/getProfileQuestionListResponse.ts
+++ src/modules/dto/response/getProfileQuestionListResponse.ts
@@ -1,4 +1,8 @@
-export interface getProfileQuestionListResponse {
+export type getProfileQuestionListResponse = ProfileQuestionType[];
+
+export type ProfileQuestionType = {
   id: number;
   video_url: string;
-}
+  comment_cnt?: number;
+  like_cnt?: number;
+};
