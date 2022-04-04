d893e66f0c0cd8f59fc218923a7bfd322d2aa776
Jun Joy <chojuneseo1@gmail.com>
2022-04-04 21:42:32 +0900

    #58 Modify saga bug

--- src/utils/saga/createRequestSaga.ts
+++ src/utils/saga/createRequestSaga.ts
@@ -1,14 +1,14 @@
-import { AxiosResponse } from 'axios';
-import storageKeys from 'constant/storageKeys';
-import { call, put } from 'redux-saga/effects';
-import localStorage from 'utils/localStorage';
+import { AxiosResponse } from "axios";
+import storageKeys from "constant/storageKeys";
+import { call, put } from "redux-saga/effects";
+import localStorage from "utils/localStorage";
 
 export default function createRequestSaga(type: any, request: any) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
-  return async function* (action: any) {
-    const accessToken = await localStorage.getItem<string>(storageKeys.accessToken);
+  return function* (action: any) {
     try {
+      const accessToken: string = yield call(localStorage.getItem, storageKeys.accessToken);
       const response: AxiosResponse<unknown> = yield call(request, accessToken, action.payload);
       yield put({
         type: SUCCESS,
