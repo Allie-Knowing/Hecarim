020d000379ea7ce6ce91ea7ce092c4c45ed585e1
Kimjimin0807 <jm004471@naver.com>
2022-04-04 22:03:02 +0900

    #62 fix getProfile

--- src/utils/api/profile/index.ts
+++ src/utils/api/profile/index.ts
@@ -1,9 +1,11 @@
 import uri from "constance/uri";
+import { profileRequest } from "modules/dto/request/profileRequest";
 import { getRequest } from "../default";
 
-export const getProfile = async (id: number) => {
+export const getProfile = async (_, body: profileRequest) => {
   const request = getRequest();
 
-  const data = await request.post(`${uri.getProfile}${id}`);
-  return data;
+  const data = await request.get(`${uri.getProfile}${body.id}`);
+  console.log(data);
+  return data.data;
 };
