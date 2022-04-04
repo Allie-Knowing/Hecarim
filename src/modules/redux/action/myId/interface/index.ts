a8d9e343c6074828ad8d090627ed65323d986859
Kimjimin0807 <jm004471@naver.com>
2022-04-04 16:02:34 +0900

    #62 Add my id action

---
+++ src/modules/redux/action/myId/interface/index.ts
@@ -0,0 +1,4 @@
+export const GET_MY_ID = "PROFILE/GET_MY_ID" as const;
+export const GET_MY_ID_FAILURE = `PROFILE/GET_MY_ID_FAILURE` as const;
+export const GET_MY_ID_SUCCESS = `PROFILE/GET_MY_ID_SUCCESS` as const;
+export const SET_ID = "PROFILE/SET_ID" as const;
