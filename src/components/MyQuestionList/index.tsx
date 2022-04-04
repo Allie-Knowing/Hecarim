f849d9c2019f80183b4383ab1637ffb0a2f46129
Kimjimin0807 <jm004471@naver.com>
2022-04-05 01:53:41 +0900

    #62 Add MyPage Api Connaction

--- src/components/MyQuestionList/index.tsx
+++ src/components/MyQuestionList/index.tsx
@@ -1,27 +1,34 @@
 import React, { FC } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, Text } from "react-native";
+import useProfile from "utils/hooks/profile/useProfile";
 import MyQuestion from "./MyQuestion";
 import * as S from "./style";
 
 const { height } = Dimensions.get("window");
 
 const MyQuestionList: FC = () => {
+  const { state } = useProfile();
+
   return (
     <S.Container height={height - 290}>
       <S.Title>나의 질문</S.Title>
-      <S.QuestionContainer
-        key={"#"}
-        data={[1, 2, 3, 4, 5, 6, 7]}
-        renderItem={() => <MyQuestion />}
-        numColumns={2}
-        showsVerticalScrollIndicator={false}
-        columnWrapperStyle={{
-          width: "100%",
-          paddingRight: 10,
-          paddingLeft: 10,
-          overflow: "hidden",
-        }}
-      />
+      {state.questionList.length === 0 ? (
+        <S.Notice>질문이 없습니다.</S.Notice>
+      ) : (
+        <S.QuestionContainer
+          key={"#"}
+          data={state.questionList}
+          renderItem={({ item }: any) => <MyQuestion question={item} />}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          columnWrapperStyle={{
+            width: "100%",
+            paddingRight: 10,
+            paddingLeft: 10,
+            overflow: "hidden",
+          }}
+        />
+      )}
     </S.Container>
   );
 };
