4d825240d60bcd689a6efaeca7cb726bec75fc1a
Jun Joy <chojuneseo1@gmail.com>
2022-04-04 20:29:19 +0900

    #58 Add create camera middleware

--- src/modules/redux/middleware/index.ts
+++ src/modules/redux/middleware/index.ts
@@ -2,7 +2,8 @@ import { all } from "redux-saga/effects";
 import feedSaga from "./feed";
 import likeSaga from "./like";
 import signinSaga from "./signin";
+import cameraSaga from "./camera";
 
 export default function* rootSaga() {
-  yield all([feedSaga(), signinSaga(), likeSaga()]);
+  yield all([feedSaga(), signinSaga(), likeSaga(), cameraSaga()]);
 }
