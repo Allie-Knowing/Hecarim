743c82ff58280c62bfd3d9edf5ebcaa75b15ee55
Kimjimin0807 <jm004471@naver.com>
2022-04-04 16:04:58 +0900

    #62 Add profile saga

--- src/modules/redux/middleware/profile/index.ts
+++ src/modules/redux/middleware/profile/index.ts
@@ -1,1 +1,12 @@
-export const getProfileSaga = function* (): any {};
+import { GET_PROFILE } from "modules/redux/action/profile/interface";
+import { takeLatest } from "redux-saga/effects";
+import { getProfile } from "utils/api/profile";
+import createRequestSaga from "utils/saga/createRequestSaga";
+
+export const getProfileSaga = createRequestSaga(GET_PROFILE, getProfile);
+
+function* profileSaga() {
+  yield takeLatest(GET_PROFILE, getProfileSaga);
+}
+
+export default profileSaga;
