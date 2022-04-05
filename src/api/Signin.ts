49aeef6f31954d6021419debd507639d32e1d489
Kimjimin0807 <jm004471@naver.com>
2022-04-06 02:07:20 +0900

    #89 Add apple oAuth api

--- src/api/Signin.ts
+++ src/api/Signin.ts
@@ -6,6 +6,7 @@ import { noTokenInstance } from "utils/axios";
 import localStorage from "utils/localStorage";
 
 export const postSigninApi = async (body: signinRequest) => {
+  console.log(body);
   let response = null;
 
   if (body.provider === "GOOGLE") {
@@ -15,14 +16,20 @@ export const postSigninApi = async (body: signinRequest) => {
         id_token: body.id_token,
       }
     );
-  } else {
+  } else if (body.provider === "NAVER") {
     response = await noTokenInstance.post<signinResponse>(
       `${uri.signin}${body.provider}`,
       {
         code: body.id_token,
       }
     );
+  } else if (body.provider === "APPLE") {
+    response = await noTokenInstance.post<signinResponse>(uri.appleSignin, {
+      id_token: body.id_token,
+    });
   }
+
+  console.log(response);
 
   await Promise.all([
     localStorage.setItem<string>(
