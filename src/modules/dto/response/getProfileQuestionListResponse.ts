fc386f8541486d185600004241bd9fa7552a338f
Kimjimin0807 <jm004471@naver.com>
2022-04-05 10:20:43 +0900

    #80 Add thumbnail

--- src/modules/dto/response/getProfileQuestionListResponse.ts
+++ src/modules/dto/response/getProfileQuestionListResponse.ts
@@ -3,6 +3,5 @@ export type getProfileQuestionListResponse = ProfileQuestionType[];
 export type ProfileQuestionType = {
   id: number;
   video_url: string;
-  comment_cnt?: number;
-  like_cnt?: number;
+  thumbnail?: string;
 };
