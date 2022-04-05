7ddead497aeed91ce58245af4d5fc6bd8bf89473
Kimjimin0807 <jm004471@naver.com>
2022-04-06 02:08:29 +0900

    #89 Fix apple oAuth button Style change

--- src/components/Buttons/Login/styles.ts
+++ src/components/Buttons/Login/styles.ts
@@ -11,3 +11,7 @@ export const Content = styled.Text`
   color: ${({ theme }) => theme.colors.grayscale.scale80};
   font-size: 14px;
 `;
+
+export const AppleWrapper = styled.View<{ width: number }>`
+  margin-top: 15px;
+`;
