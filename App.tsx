7f343d04cddc196a6346c741274c95fb6c6d3cc4
Jun Joy <chojuneseo1@gmail.com>
2022-04-06 13:29:43 +0900

    #91 Create Camera Modal

--- App.tsx
+++ App.tsx
@@ -7,10 +7,7 @@ import { useFonts } from "expo-font";
 import AppLoading from "expo-app-loading";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
-import {
-  CardStyleInterpolators,
-  createStackNavigator,
-} from "@react-navigation/stack";
+import { CardStyleInterpolators, createStackNavigator } from "@react-navigation/stack";
 import StackedQuestionList from "screens/StackedQuestionList";
 import { Host } from "react-native-portalize";
 import { MainStackParamList } from "hooks/useMainStackNavigation";
@@ -21,6 +18,10 @@ import theme from "theme/theme";
 import Setting from "screens/Setting";
 import UserPage from "screens/MyPage/UserPage";
 import { QueryClient, QueryClientProvider } from "react-query";
+import CameraComponent from "components/Question/Camera";
+import isStackContext from "./src/context/IsStackContext";
+import CameraProvider from "context/CameraContext";
+import CameraDetail from "components/Question/CameraDetail";
 
 const Root = createStackNavigator<MainStackParamList>();
 
@@ -47,39 +48,58 @@ export default function App() {
                 <AlretProvider>
                   <NavigationContainer>
                     <Host>
-                      <Root.Navigator
-                        initialRouteName="Main"
-                        screenOptions={{
-                          cardStyleInterpolator:
-                            CardStyleInterpolators.forHorizontalIOS,
-                        }}
-                      >
-                        <Root.Screen
-                          name="Main"
-                          component={BottomTabNavigation}
-                          options={{ headerShown: false }}
-                        />
-                        <Root.Screen
-                          name="StackedQuestionList"
-                          component={StackedQuestionList}
-                          options={{ headerShown: false }}
-                        />
-                        <Root.Screen
-                          name="Login"
-                          component={Login}
-                          options={{ headerShown: false }}
-                        />
-                        <Root.Screen
-                          name="Setting"
-                          component={Setting}
-                          options={{ title: "설정" }}
-                        />
-                        <Root.Screen
-                          name="UserPage"
-                          component={UserPage}
-                          options={{ headerShown: false }}
-                        />
-                      </Root.Navigator>
+                      <CameraProvider>
+                        <Root.Navigator
+                          initialRouteName="Main"
+                          screenOptions={{
+                            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                          }}
+                        >
+                          <Root.Screen
+                            name="Main"
+                            component={BottomTabNavigation}
+                            options={{ headerShown: false }}
+                          />
+                          <Root.Screen
+                            name="StackedQuestionList"
+                            component={StackedQuestionList}
+                            options={{ headerShown: false }}
+                          />
+                          <Root.Screen
+                            name="Login"
+                            component={Login}
+                            options={{ headerShown: false }}
+                          />
+                          <Root.Screen
+                            name="Setting"
+                            component={Setting}
+                            options={{ title: "설정" }}
+                          />
+                          <Root.Screen
+                            name="UserPage"
+                            component={UserPage}
+                            options={{ headerShown: false }}
+                          />
+                          <Root.Screen
+                            name="CameraPage"
+                            component={() => (
+                              <isStackContext.Provider value={true}>
+                                <CameraComponent />
+                              </isStackContext.Provider>
+                            )}
+                            options={{ headerShown: false }}
+                          />
+                          <Root.Screen
+                            name="CameraDetail"
+                            component={() => (
+                              <isStackContext.Provider value={true}>
+                                <CameraDetail />
+                              </isStackContext.Provider>
+                            )}
+                            options={{ headerShown: false }}
+                          />
+                        </Root.Navigator>
+                      </CameraProvider>
                     </Host>
                   </NavigationContainer>
                 </AlretProvider>
