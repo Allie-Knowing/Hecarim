b7e914d9f5e4093b875def4ced7ce50dce1f7689
Jun Joy <chojuneseo1@gmail.com>
2022-05-03 15:56:59 +0900

    Create Status in Uploading Modal

--- src/components/Question/UploadingModal/index.tsx
+++ src/components/Question/UploadingModal/index.tsx
@@ -2,11 +2,16 @@ import { FC } from "react";
 import * as S from "./styles";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const UploadingModal: FC = (): JSX.Element => {
+interface Props {
+  status: number;
+}
+
+const UploadingModal: FC<Props> = ({ status }): JSX.Element => {
   const { top: TOP_PAD } = useSafeAreaInsets();
   return (
     <S.UploadingContainer topPad={TOP_PAD}>
       <S.UploadingText>업로딩 중</S.UploadingText>
+      <S.UploadingStatus>{status}%</S.UploadingStatus>
     </S.UploadingContainer>
   );
 };
--- src/components/Question/UploadingModal/styles.ts
+++ src/components/Question/UploadingModal/styles.ts
@@ -4,18 +4,25 @@ export const UploadingContainer = styled.View<{ topPad: number }>`
   position: absolute;
   left: 0;
   top: ${(props) => props.topPad + 130};
-  width: 100;
+  width: 120;
   height: 40;
   background-color: ${({ theme }) => theme.colors.primary.default};
   z-index: 99;
   border-top-right-radius: 10;
   border-bottom-right-radius: 10;
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
 `;
 
 export const UploadingText = styled.Text`
   font: ${({ theme }) => theme.fonts.body1};
+  color: ${({ theme }) => theme.colors.grayscale.scale10};
+  margin-right: 5px;
+`;
+
+export const UploadingStatus = styled.Text`
+  font: ${({ theme }) => theme.fonts.body2};
   color: ${({ theme }) => theme.colors.grayscale.scale10};
 `;
