d9a0550c2f52db7c544b65355f089721f7f4dd85
Kimjimin0807 <jm004471@naver.com>
2022-05-03 16:47:27 +0900

    #130 Add IQ History Publishing

---
+++ src/components/Wallet/IQHistory/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import * as S from "./style";
+
+const IQHistory: FC = () => {
+  return (
+    <S.IQHistoryContainer>
+      <S.Title>IQ 내역</S.Title>
+      
+    </S.IQHistoryContainer>
+  );
+};
+
+export default IQHistory;
