80021a88991e034dd73b701a4fd4dff8bb2c9998
Jun Joy <chojuneseo1@gmail.com>
2022-05-03 15:55:58 +0900

    Modify add onUploadingStatus

--- src/queries/useVideoUrl.ts
+++ src/queries/useVideoUrl.ts
@@ -1,5 +1,16 @@
-import { getVideoUrl } from "utils/api/videoUrl";
 import { useMutation } from "react-query";
+import { useContext } from "react";
+import { UploadingStatusContext } from "context/UploadingStatusContext";
+import uri from "constance/uri";
+import { instance } from "utils/axios";
+
+interface responseType {
+  data: {
+    data: {
+      url: string;
+    };
+  };
+}
 
 interface mutateType {
   type: "question" | "answer";
@@ -7,7 +18,31 @@ interface mutateType {
 }
 
 export const useVideoUrlMutation = () => {
-  const videoUrl = useMutation((mutation: mutateType) => getVideoUrl(mutation.type, mutation.file));
+  const { setStatus } = useContext(UploadingStatusContext);
+
+  const getVideoUrl = async (
+    type: "question" | "answer",
+    file: string | FormData
+  ) => {
+    const data: responseType = await instance.post(`${uri.file}`, file, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      params: {
+        type,
+      },
+      onUploadProgress: (progressEvent) => {
+        const { loaded, total } = progressEvent;
+        setStatus(Math.ceil((loaded * 100) / total));
+      },
+    });
+
+    return data;
+  };
+
+  const videoUrl = useMutation((mutation: mutateType) =>
+    getVideoUrl(mutation.type, mutation.file)
+  );
 
   return { videoUrl };
 };
