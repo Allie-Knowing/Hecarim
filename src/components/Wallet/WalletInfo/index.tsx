ad66fbc605d45443d1fa033be0b19ef18b68fcfb
Kimjimin0807 <jm004471@naver.com>
2022-05-03 21:54:00 +0900

    #130 Fix style

--- src/components/Wallet/WalletInfo/index.tsx
+++ src/components/Wallet/WalletInfo/index.tsx
@@ -9,44 +9,42 @@ const TierImage = require("assets/tier.png");
 const WalletInfo: FC = () => {
   const { top: topPad } = useSafeAreaInsets();
 
-  console.log(topPad);
-
   return (
     <S.WalletInfoContainer topPad={topPad}>
-        <S.WalletHeader>
-          <S.Logo source={Logo} />
-          <S.HeaderTitle>지갑</S.HeaderTitle>
-        </S.WalletHeader>
-        <S.TierContainer>
-          <S.TireInfo>
-            <S.TierImage source={TierImage} />
-            <S.TireTitle>챌린저</S.TireTitle>
-          </S.TireInfo>
-          <S.ShowTireButton>
-            <S.ShowTireButtonDescription>등급보기</S.ShowTireButtonDescription>
-          </S.ShowTireButton>
-        </S.TierContainer>
-        <S.Accumulate>
-          <S.AccumulateContent>
-            <S.AccumulateTitle>누적 IQ</S.AccumulateTitle>
-            <S.AccumulateValue>999999 IQ</S.AccumulateValue>
-          </S.AccumulateContent>
-          <S.AccumulateContent>
-            <S.AccumulateTitle>채택 수</S.AccumulateTitle>
-            <S.AccumulateValue>99999</S.AccumulateValue>
-          </S.AccumulateContent>
-        </S.Accumulate>
-        <S.HoldingContainer>
-          <S.HoldingContent>
-            <S.HoldingTitle>보유 IQ</S.HoldingTitle>
-            <S.HoldingValue>99999 IQ</S.HoldingValue>
-          </S.HoldingContent>
-          <S.HoldingLine />
-          <S.HoldingContent>
-            <S.HoldingTitle>오늘 활동점수</S.HoldingTitle>
-            <S.HoldingValue>99</S.HoldingValue>
-          </S.HoldingContent>
-        </S.HoldingContainer>
+      <S.WalletHeader>
+        <S.Logo source={Logo} />
+        <S.HeaderTitle>지갑</S.HeaderTitle>
+      </S.WalletHeader>
+      <S.TierContainer>
+        <S.TireInfo>
+          <S.TierImage source={TierImage} />
+          <S.TireTitle>챌린저</S.TireTitle>
+        </S.TireInfo>
+        <S.ShowTireButton>
+          <S.ShowTireButtonDescription>등급보기</S.ShowTireButtonDescription>
+        </S.ShowTireButton>
+      </S.TierContainer>
+      <S.Accumulate>
+        <S.AccumulateContent>
+          <S.AccumulateTitle>누적 IQ</S.AccumulateTitle>
+          <S.AccumulateValue>999999 IQ</S.AccumulateValue>
+        </S.AccumulateContent>
+        <S.AccumulateContent>
+          <S.AccumulateTitle>채택 수</S.AccumulateTitle>
+          <S.AccumulateValue>99999</S.AccumulateValue>
+        </S.AccumulateContent>
+      </S.Accumulate>
+      <S.HoldingContainer>
+        <S.HoldingContent>
+          <S.HoldingTitle>보유 IQ</S.HoldingTitle>
+          <S.HoldingValue>99999 IQ</S.HoldingValue>
+        </S.HoldingContent>
+        <S.HoldingLine />
+        <S.HoldingContent>
+          <S.HoldingTitle>오늘 활동점수</S.HoldingTitle>
+          <S.HoldingValue>99</S.HoldingValue>
+        </S.HoldingContent>
+      </S.HoldingContainer>
     </S.WalletInfoContainer>
   );
 };
