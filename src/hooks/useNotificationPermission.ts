f13f85e5d18802df7825b246f55dbcce7b8e252f
Kimjimin0807 <jm004471@naver.com>
2022-05-12 14:02:22 +0900

    #144 Fix debug Code delete

--- src/hooks/useNotificationPermission.ts
+++ src/hooks/useNotificationPermission.ts
@@ -1,25 +1,23 @@
 import { useEffect, useState } from "react";
-import * as Notifications from 'expo-notifications';
+import * as Notifications from "expo-notifications";
 
 export const useNotificationPermission = () => {
-    const [token, setToken] = useState<string>(undefined);
+  const [token, setToken] = useState<string>(undefined);
 
-    const askNotificationPermission = async () => {
-        const { status: currentStatus } = await Notifications.requestPermissionsAsync();
-        return currentStatus;
-    }
+  const askNotificationPermission = async () => {
+    const { status: currentStatus } =
+      await Notifications.requestPermissionsAsync();
+    return currentStatus;
+  };
 
-    useEffect(() => {
-        (
-            async () => {
-                let { status } = await Notifications.getPermissionsAsync();
-                if(status !== "granted") status = await askNotificationPermission();
-                if(status !== "granted") return alert("알림을 활성 해주세요 !");
-                const token = (await Notifications.getExpoPushTokenAsync()).data;
-                setToken(token);
-            }
-        )();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Notifications.getPermissionsAsync();
+      if (status !== "granted") status = await askNotificationPermission();
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      setToken(token);
+    })();
+  }, []);
 
-    return { token, isGranted: token !== undefined, askNotificationPermission };
-}
+  return { token, isGranted: token !== undefined, askNotificationPermission };
+};
