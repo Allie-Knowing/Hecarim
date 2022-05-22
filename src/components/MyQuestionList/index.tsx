74fa2f9210d7ec369a46b540e6889c0b3453ae66
Kimjimin0807 <jm004471@naver.com>
2022-05-22 22:33:28 +0900

    #141 Fix userPage Answer Stacked

--- src/components/MyQuestionList/index.tsx
+++ src/components/MyQuestionList/index.tsx
@@ -36,9 +36,9 @@ const MyQuestionList: FC<Props> = ({
 
   const moveQuestionStack = (index: number) => {
     if (profileDataList.length <= 0) return;
-    const navegationArray: Question[] = [];
+    const navigationArray: Question[] = [];
     for (let i = 0; i < profileDataList.length; i++) {
-      navegationArray.push({
+      navigationArray.push({
         id: parseInt(profileDataList[i].id),
         description: profileDataList[i].video_description,
         title: profileDataList[i].video_title,
@@ -53,6 +53,10 @@ const MyQuestionList: FC<Props> = ({
         is_adoption: profileDataList[i].is_adoption,
       });
     }
+    navigation.push("StackedQuestionList", {
+      data: navigationArray,
+      index,
+    });
   };
 
   useEffect(() => {
@@ -109,3 +113,33 @@ const MyQuestionList: FC<Props> = ({
 };
 
 export default MyQuestionList;
+
+// comment_cnt: 1
+// created_at: "2022-05-12T14:29:30.467Z"
+// id: 307
+// is_adoption: 0
+// is_like: true
+// is_mine: true
+// like_cnt: 7
+// thumbnail: "https://test-knowing.s3.ap-northeast-2.amazonaws.com/processed/question/mov_b25d8beb-0374-4255-9d48-ddd360842750knowing.png"
+// user_id: 16
+// user_profile: "https://s3.ap-northeast-2.amazonaws.com/test-knowing/profile/58f95f13-cdd0-4b73-b1cc-88221d114e8d.jpg"
+// video_description: "스키타러 가고 싶어요 ㅜㅜ"
+// video_title: "어디 스키장인가요??"
+// video_url: "https://d2kugok170b34v.cloudfront.net/processed/question/mov_b25d8beb-0374-4255-9d48-ddd360842750/mov_b25d8beb-0374-4255-9d48-ddd360842750knowing.m3u8"
+// views: "90"
+
+// comment_cnt: 0
+// created_at: "2022-04-21T03:57:46.642Z"
+// id: 197
+// is_adoption: 0
+// is_like: true
+// is_mine: false
+// like_cnt: 5
+// thumbnail: "https://test-knowing.s3.ap-northeast-2.amazonaws.com/processed/question/mp4_31730040-6925-4f63-b911-d17a745e525fknowing.png"
+// user_id: 3
+// user_profile: "https://ssl.pstatic.net/static/pwe/address/img_profile.png"
+// video_description: "롤체 저티어를 벗어나덜 몬하고 있어요 도와주세요"
+// video_title: "롤체 넘나 어려웡"
+// video_url: "https://d2kugok170b34v.cloudfront.net/processed/question/mp4_31730040-6925-4f63-b911-d17a745e525f/mp4_31730040-6925-4f63-b911-d17a745e525fknowing.m3u8"
+// views: "1"
