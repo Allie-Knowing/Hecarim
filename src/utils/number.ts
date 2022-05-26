e5d9b70903c749d533d80e74af2310521c2ce06d
Sung Ye In <syi9397@naver.com>
2022-05-26 22:48:51 +0900

    Fix number utils

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,7 +1,7 @@
 export const reduceNumberLength = (n: number) => {
     const len = n.toString().length;
     if(len >= 9) return `${n / 100000000}억`;
-    else if(len >= 5) return `${n / 10000}억`;
-    else if(len >= 4) return `${n / 1000}억`;
+    else if(len >= 5) return `${n / 10000}만`;
+    else if(len >= 4) return `${n / 1000}천`;
     else return n;
 }
